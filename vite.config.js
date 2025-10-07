e711eb6575e3681c985c2fd580d2e349ff7e1c3a
Diego Caponera <diego.caponera@gmail.com>
2025-10-07 21:24:28 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import markdown, { Mode } from "vite-plugin-markdown";
 
 export default defineConfig({
+  base: '/villa-lobos-trainer/',
   plugins: [
     react(),
     markdown({
@@ -10,3 +11,4 @@ export default defineConfig({
     }),
   ],
 });
+
